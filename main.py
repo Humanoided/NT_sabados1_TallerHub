1abce0d3dd671296f7f9a578a978d10c3423f358
Julian Ruiz Medina <jurumello0009@gmail.com>
2026-04-25 09:43:41 -0500

    se agrega descrpcion

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ from notebook.limpieza import limpiar_users
 
 from simulacion_users import simular_users
 
+from notebook.descripcion import describir_users
+
 users=simular_users(100)
+
 users_ordenados=pd.DataFrame(users)
+
 users_limpios=limpiar_users(users_ordenados)
-print(users_limpios)
+
+describir_users(users_limpios)  
